55471b5bfabd77b149f70fe81b68a4ddcf0d4c7b
Jouko Johansson <joukojo@gmail.com>
2012-01-31 16:11:59 +0200

    the main function calls the frame creation

---
+++ httpmonitor-gui/src/main/java/com/yogocodes/httpmonitor/gui/HttpMonitorApp.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package com.yogocodes.httpmonitor.gui;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.yogocodes.httpmonitor.gui.frames.HttpMonitorAppFrame;
+
+/**
+ *  HttpMonitor application which is used to start the gui interface.
+ * 
+ * @author joukojo
+ *
+ */
+public class HttpMonitorApp {
+
+	/**
+	 * Main method
+	 * @param args cli arguments
+	 */
+	public static void main(String[] args) {
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+					
+					new HttpMonitorAppFrame();
+					
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InstantiationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (UnsupportedLookAndFeelException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				
+			}
+		});
+
+	}
+
+}
