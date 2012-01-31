37bd2c494db453b7fb8413c58e76869090617fd0
Jouko Johansson <joukojo@gmail.com>
2012-01-31 16:09:46 +0200

    application JFrame

---
+++ httpmonitor-gui/src/main/java/com/yogocodes/httpmonitor/gui/frames/HttpMonitorAppFrame.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.yogocodes.httpmonitor.gui.frames;
+
+import javax.swing.JFrame;
+
+/**
+ * @author joukojo
+ *
+ */
+public class HttpMonitorAppFrame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	public HttpMonitorAppFrame() {
+		super("HttpMonitorApplication");
+		setSize(500, 500);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+		setEnabled(true);
+	}
+	
+
+}
