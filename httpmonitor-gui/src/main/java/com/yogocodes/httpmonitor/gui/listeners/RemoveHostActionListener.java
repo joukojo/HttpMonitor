4c21e33ca39851a3423c40da2d50a7a3493684c6
Jouko Johansson <joukojo@gmail.com>
2012-02-06 22:19:53 +0200

    revised package

---
+++ httpmonitor-gui/src/main/java/com/yogocodes/httpmonitor/gui/listeners/RemoveHostActionListener.java
@@ -0,0 +1,34 @@
+package com.yogocodes.httpmonitor.gui.listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.yogocodes.httpmonitor.gui.form.HttpMonitorAppForm;
+import com.yogocodes.httpmonitor.gui.form.HttpMonitorAppFormFactory;
+import com.yogocodes.httpmonitor.gui.frames.HttpMonitorAppFrameFactory;
+
+public class RemoveHostActionListener implements ActionListener {
+
+	private final static Logger LOG = LoggerFactory.getLogger(RemoveHostActionListener.class);
+
+	@Override
+	public void actionPerformed(final ActionEvent e) {
+		final HttpMonitorAppForm appFormInstance = HttpMonitorAppFormFactory.getAppFormInstance();
+		final int selectedIndex = appFormInstance.getMonitoredHostList().getSelectedIndex();
+
+		if (selectedIndex != -1) {
+			appFormInstance.getMonitorTargets().remove(selectedIndex);
+
+			appFormInstance.refreshHostList();
+			appFormInstance.getMonitoredHostList().validate();
+			appFormInstance.getMonitoredHostList().repaint();
+			HttpMonitorAppFrameFactory.getAppFrameInstance().repaint();
+		} else {
+			LOG.debug("tried to remove non-selected item {}", selectedIndex);
+		}
+	}
+
+}
